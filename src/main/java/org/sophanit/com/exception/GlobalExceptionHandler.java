616cca0cfbc88e63f23a7f10e86811732438b71e
sophanit <soeuk.sophanit2001@gmail.com>
2024-04-07 10:07:43 +0700

    change relation to join many to many in EventRepo

--- src/main/java/org/sophanit/com/exception/GlobalExceptionHandler.java
+++ src/main/java/org/sophanit/com/exception/GlobalExceptionHandler.java
@@ -16,7 +16,7 @@ public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
     @ExceptionHandler(ApiException.class)
     ProblemDetail handleExceptions(ApiException exception) {
 
-        ProblemDetail problemDetail = ProblemDetail.forStatusAndDetail(HttpStatus.NOT_FOUND,
+        ProblemDetail problemDetail = ProblemDetail.forStatusAndDetail(exception.getHttpStatus(),
                 exception.getMessage());
         problemDetail.setType(URI.create("about:blank"));
         problemDetail.setProperty("title",exception.getMessage());
